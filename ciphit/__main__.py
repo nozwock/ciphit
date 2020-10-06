aa0245aa5c7a05d637ab4e6977b6c8b2f701f927
Sagar Kumar <57829219+sgrkmr@users.noreply.github.com>
2020-10-06 16:19:34 +0530

    feat: Add support for files

--- ciphit/__main__.py
+++ ciphit/__main__.py
@@ -1,10 +1,17 @@
 import click
-from click_option_group import optgroup, RequiredMutuallyExclusiveOptionGroup
-try:
-    from rich import print
-except ImportError as e:
-    print(repr(e))
+from click_option_group import (
+    optgroup, 
+    MutuallyExclusiveOptionGroup,
+    RequiredMutuallyExclusiveOptionGroup,
+)
+from rich import print
 from ciphit.basemods.Ciphers import aes
+
+def get_singleline(multi: list) -> repr:
+    return repr('\n'.join([i.strip('\n') for i in multi]))
+
+def get_multiline(single: str) -> str:
+    return '\n'.join(single.strip("'").split("\\n"))
 
 def print_help():
     ctx = click.get_current_context()
@@ -12,52 +19,69 @@ def print_help():
 
 @click.command()
 @optgroup.group(
-        "Mutually Exclusive",
+        "Encode/Decode", 
         cls=RequiredMutuallyExclusiveOptionGroup,
 )
 @optgroup.option(
-        "-e", "--encode",
-        is_flag=True,
-        flag_value=True,
+        "-e", "--encode", is_flag=True, flag_value=True,
 )
 @optgroup.option(
-        "-d", "--decode",
-        is_flag=True,
-        flag_value=True,
+        "-d", "--decode", is_flag=True, flag_value=True,
 )
 @click.option(
-        "-t", "--text",
-        default=False,
-        type=str,
-)
-@click.option(
-        "-k", "--key",
-        prompt=True,
-        hide_input=True,
+        "-k","--key", 
+        help="The key with which text is Encoded/Decoded.", 
+        prompt=True, 
+        hide_input=True, 
         confirmation_prompt=True,
+)
+@optgroup.group(
+        "Text/File", 
+        cls=MutuallyExclusiveOptionGroup,
+)
+@optgroup.option(
+    "-t", "--text",
+    help="The text you want to Encode/Decode.", 
+    default=False, type=str,
+)
+@optgroup.option(
+    "-f", "--file", 
+    type=click.File('r+'), 
+    default=None,
 )
 def main(**kwargs):
 
-    text = kwargs['text']
-    if not text:
-        print("[blue]Opening editor[/blue]")
+    isFile = not kwargs['file'] is None
+    if not kwargs['text'] and not isFile:
+        print("[bold blue]Opening editor[/bold blue]")
         click.pause()
-        text=click.edit(text='')
-        if text in (None,''):
-            print("[bold red]error:[/bold red] empty text")
+        kwargs['text']=click.edit(text='')
+        if kwargs['text'] in (None,''):
+            print("[bold red]error:[/bold red] Text is empty.")
             exit(-1)
-        text=text.strip('\n')
+        kwargs['text']=kwargs['text'].strip('\n')
 
     cpt = aes.Crypt()
-    encode, decode, key= \
-    kwargs['encode'], kwargs['decode'], kwargs['key']
-    if encode:
-        _ = cpt.Encode(text,key)
-        print(f"[bold yellow]{_}[/bold yellow]")
-    elif decode:
-        _ = cpt.Decode(text,key)
-        print(f"[bold green]{_}[/bold green]")
+    if not isFile:
+        if kwargs['encode']:
+            _ = cpt.Encode(kwargs['text'],kwargs['key'])
+            print(f"Final result: [bold yellow]{_}[/bold yellow]")
+        else:#kwargs['decode']
+            _ = cpt.Decode(kwargs['text'],kwargs['key'])
+            print(f"Final result: [bold green]{_}[/bold green]")
+    elif isFile:
+        if kwargs['encode']:
+            _ = cpt.Encode(get_singleline(kwargs['file'].readlines()), kwargs['key'])
+            msg="[bold orange]File is now enncrypted.[/bold orange]"
+        else:#kwargs['decode']
+            _ = get_multiline(cpt.Decode(kwargs['file'].read().strip(), kwargs['key']))
+            msg="[bold green]File is now decrypted.[/bold green]"
+        kwargs['file'].truncate(0)
+        kwargs['file'].seek(0)
+        kwargs['file'].write(_)
+        print(msg)
     else:
+        #this condition never occurs, for now.
         print_help()
         exit(-1)
 
