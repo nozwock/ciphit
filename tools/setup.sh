2561d635aee40372baeb1ba535170325c4d2f19a
Sagar Kumar <57829219+sgrkmr@users.noreply.github.com>
2020-10-27 23:10:13 +0530

    dev: Update setup.sh (setup_color)

--- tools/setup.sh
+++ tools/setup.sh
@@ -18,12 +18,12 @@ error() {
 setup_color() {
 	# Only use colors if connected to a terminal
 	if [ -t 1 ]; then
-		RED=$(printf '\033[31m')
-		GREEN=$(printf '\033[32m')
-		YELLOW=$(printf '\033[33m')
-		BLUE=$(printf '\033[34m')
-		BOLD=$(printf '\033[1m')
-		RESET=$(printf '\033[m')
+		RED='\033[1;31m'
+		GREEN='\033[1;32m'
+		YELLOW='\033[1;33m'
+		BLUE='\033[1;34m'
+		BOLD=''
+		RESET='\033[m'
 	else
 		RED=""
 		GREEN=""
